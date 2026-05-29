fd96fc236f04bc8199ea786e81f1645c32e10b69
baruni8march <golumolu30june@gmail.com>
2026-05-29 23:54:01 +0600

    more

--- DAY6/PART2/database.js
+++ DAY6/PART2/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 //This imports SQLite
-const db = new sqlite3.Database("./todos.db", (err) => {
+const db = new sqlite3.Database("./todos.db", (err) => { //this connects with database
     if (err) {
         console.log("Database connection failed");
         console.log(err.message);
@@ -10,11 +10,35 @@ const db = new sqlite3.Database("./todos.db", (err) => {
 });
 
 db.run(`
-    CREATE TABLE IF NOT EXISTS todos (
+    CREATE TABLE IF NOT EXISTS todos ( 
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         completed BOOLEAN DEFAULT 0
     )
 `);
+// `......` is a template literal string
+
+/*CREATE TABLE IF NOT EXISTS todos (...):
+Create todos table only if it does not already exist.
+*/
+
+/*
+id INTEGER PRIMARY KEY AUTOINCREMENT.Means:
+id is number
+unique for every row
+auto increases: 1, 2, 3...
+*/
+
+/*
+title TEXT NOT NULL.Means:
+title is text
+cannot be empty
+*/
+
+/*
+completed BOOLEAN DEFAULT 0.Means:
+completed is true/false style
+default is 0, meaning false
+*/
 
 module.exports = db;
--- DAY6/PART2/server.js
+++ DAY6/PART2/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const db = require("./database"); //v.v.imp
+const db = require("./database");
 
 const app = express();
 
@@ -8,6 +8,56 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("SQLite Todo API running");
+});
+
+app.get("/todos", (req, res) => {
+    db.all("SELECT * FROM todos", (err, rows) => {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Database error"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            todos: rows
+        });
+    });
+});
+
+app.post("/todos", (req, res) => {
+    const title = req.body.title;
+
+    if (!title) {
+        return res.status(400).json({
+            success: false,
+            message: "Title is required"
+        });
+    }
+
+    db.run(
+        "INSERT INTO todos (title, completed) VALUES (?, ?)",
+        [title, 0],
+        function (err) {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: "Database insert failed"
+                });
+            }
+
+            res.status(201).json({
+                success: true,
+                message: "Todo created",
+                todo: {
+                    id: this.lastID,
+                    title: title,
+                    completed: 0
+                }
+            });
+        }
+    );
 });
 
 app.listen(6000, () => {
